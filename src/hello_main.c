2c8cd956ea3b9edbb2b9839581f99c5dc365613e
missyaria <93721556+missyaria@users.noreply.github.com>
2023-12-01 12:05:17 +0100

    Update hello_main.c
    Added the possibility to type both first and last name as an input

--- src/hello_main.c
+++ src/hello_main.c
@@ -3,9 +3,12 @@
 #include <string.h>
 
 int main () {
+	char * prenom[60];
 	char * nom[60];
+	printf("Entrez votre prenom : \n");
+	scanf("%s", prenom);
 	printf("Entrez votre nom : \n");
 	scanf("%s", nom);
-	printf("Bonjour %s.\n", nom);
+	printf("Bonjour %s %s.\n", prenom, nom);
 exit(EXIT_SUCCESS);
 }
